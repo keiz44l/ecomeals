1528922abc0cf9a681576402fef1c84c25f27c0a
keiz44l <80106527+keiz44l@users.noreply.github.com>
2024-05-17 13:45:00 +0200

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"ecomeal-8d6a3","appId":"1:187674353853:web:374d62b4f1f8715fc5d7d4","storageBucket":"ecomeal-8d6a3.appspot.com","apiKey":"AIzaSyAkwJdQhqWJCIq75oyMmO6CU-u2e9ugmIc","authDomain":"ecomeal-8d6a3.firebaseapp.com","messagingSenderId":"187674353853"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const encironment = {
+    production: false,
+};
